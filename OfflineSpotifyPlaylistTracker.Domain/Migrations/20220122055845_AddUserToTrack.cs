4a736530c9861d55e0fc2300038c2a485657d1ef
Alex Barker <alexbark@gmail.com>
2022-01-22 16:59:40 +1100

    add user to track

---
+++ OfflineSpotifyPlaylistTracker.Domain/Migrations/20220122055845_AddUserToTrack.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfflineSpotifyPlaylistTracker.Domain.Migrations
+{
+    public partial class AddUserToTrack : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Tracks",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_UserId",
+                table: "Tracks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tracks_Users_UserId",
+                table: "Tracks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tracks_Users_UserId",
+                table: "Tracks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tracks_UserId",
+                table: "Tracks");
+
+            migrationBuilder.UpdateData(
+                table: "Tracks",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Tracks",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- OfflineSpotifyPlaylistTracker.Domain/Models/Track.cs
+++ OfflineSpotifyPlaylistTracker.Domain/Models/Track.cs
@@ -7,6 +7,6 @@
         public string Artist { get; set; }
         public string FileName { get; set; }
         public string AlbumArt { get; set; }
-        public string UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
